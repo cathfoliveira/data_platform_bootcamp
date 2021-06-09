b62120a5752e6734bd5e2f431d0d5051625f09f6
Catharina Oliveira <catharinafoliveira@gmail.com>
2021-06-09 19:02:14 -0300

    feat(add common stack): including RDS and a VPC

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
+from data_platform.data_lake.stack import CommonStack
 
 app = core.App()
-data_lake = DataLakeStack(app) # Instanciando a stack
+data_lake = DataLakeStack(app)      # Instanciando a stack (Estrutura do data lake)
+common_stack = CommonStack(app)     # Instanciando a VPC, políticas, BD PostgreSQL, gateways etc. (RDS + VPC)
 app.synth()
