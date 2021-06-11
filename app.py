941d66bcf0c6750db6e4c399018e3d5be00f38cf
Catharina Oliveira <catharinafoliveira@gmail.com>
2021-06-11 02:43:18 -0300

    feat(add kinesis dms files)

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from aws_cdk import core
 from data_platform.data_lake.stack import DataLakeStack
 from data_platform.common_stack import CommonStack
 from data_platform.dms.stack import DmsStack
+from data_platform.kinesis.stack import KinesisStack
 
 app = core.App()
 
 data_lake = DataLakeStack(app)      # Instanciando a stack (Estrutura do data lake)
 common_stack = CommonStack(app)     # Instanciando a VPC, políticas, BD PostgreSQL, gateways etc. (RDS + VPC)
 dms = DmsStack(app,common_stack=common_stack,data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
-# Common_stack pega a vpc e a instância do RDS e o datalake pega o bucket de destino
+                                    # Common_stack pega a vpc e a instância do RDS e o datalake pega o bucket de destino
+kinenis = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 
 app.synth()
